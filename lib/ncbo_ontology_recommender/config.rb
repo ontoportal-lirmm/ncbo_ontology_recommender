3f0dda2589a25f5d5b98d47ca4072f9a6fa02162
Marcos Martinez <marcos.martinez.romero@gmail.com>
2015-03-05 18:58:01 -0800

    Set default values for input parameters

--- lib/ncbo_ontology_recommender/config.rb
+++ lib/ncbo_ontology_recommender/config.rb
@@ -15,7 +15,39 @@ module OntologyRecommender
 
     yield @settings if block_given?
 
-
+    # Set defaults
+    # The URL for the BioPortal Rails UI
+    @settings.ui_url ||= 'http://bioportal.bioontology.org/'
+    @settings.input_type ||= 1
+    @settings.output_type ||= 1
+    @settings.delimiter ||= ','
+    # Coverage evaluation
+    @settings.wc ||= 0.55
+    @settings.pref_score ||= 10
+    @settings.syn_score ||= 5
+    @settings.multiterm_score ||= 4
+    # Specialization evaluation
+    @settings.ws ||= 0.15
+    # Acceptance evaluation
+    @settings.wa ||= 0.15
+    @settings.w_bp ||= 0.34
+    @settings.w_umls ||= 0.33
+    @settings.w_pmed ||= 0.33
+    # @settings.bp_data_file ||= 'config/acceptance_evaluation/sources/bp_data.dat'
+    # @settings.umls_data_file ||= 'config/acceptance_evaluation/sources/umls_data.dat'
+    # @settings.pmed_data_file ||= 'config/acceptance_evaluation/sources/pmed_data.dat'
+    # @settings.bp_scores_file ||= 'config/acceptance_evaluation/scores/bp_scores.dat'
+    # @settings.umls_scores_file ||= 'config/acceptance_evaluation/scores/umls_scores.dat'
+    # @settings.pmed_scores_file ||= 'config/acceptance_evaluation/scores/pmed_scores.dat'
+    # Detail evaluation
+    @settings.wd ||= 0.15
+    @settings.top_defs ||= 1
+    @settings.top_syns ||= 3
+    @settings.top_props ||= 17
+    # Other parameters
+    @settings.max_elements_set ||= 3
+    @settings.max_results_single ||= 25
+    @settings.max_results_sets ||= 25
 
   end
 
