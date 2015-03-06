6522e835dc005fbaa78bbbf9a8c9153113e04dbd
Marcos Martinez <marcos.martinez.romero@gmail.com>
2015-03-05 16:09:37 -0800

    Update tests to work with new CustomAnnotation class

--- test/utils/test_annotator_utils.rb
+++ test/utils/test_annotator_utils.rb
@@ -15,33 +15,33 @@ class TestAnnotatorUtils < TestCase
     # TODO
   end
 
-  # def test_get_keyword_annotations
-  #   input = 'melanoma, white blood cell, arm, cavity of stomach, melanoma'
-  #   delimiter = ','
-  #   a1 = @@custom_annotation.new(1, 8, 'PREF', 'MELANOMA', 'o1_uri', 'o1_acronym', 'o1_uri/melanoma', nil)
-  #   a2 = @@custom_annotation.new(11, 26, 'PREF', 'WHITE BLOOD CELL', 'o1_uri', 'o1_acronym', 'o1_uri/wbc', nil)
-  #   a3 = @@custom_annotation.new(29, 31, 'SYN', 'ARM', 'o1_uri', 'o1_acronym', 'o1_uri/arm', nil)
-  #   a4 = @@custom_annotation.new(17, 26, 'SYN', 'BLOOD CELL', 'o2_uri', 'o2_acronym', 'o2_uri/bc', nil)
-  #   a5 = @@custom_annotation.new(17, 21, 'PREF', 'BLOOD', 'o3_uri', 'o3_acronym', 'o3_uri/blood', nil)
-  #   a6 = @@custom_annotation.new(34, 50, 'PREF', 'CAVITY OF STOMACH', 'o2_uri', 'o2_acronym', 'o2_uri/cos', nil)
-  #   a7 = @@custom_annotation.new(53, 60, 'PREF', 'MELANOMA', 'o1_uri', 'o1_acronym', 'o1_uri/melanoma', nil)
-  #   anns = OntologyRecommender::Utils::AnnotatorUtils.get_keyword_annotations(input, delimiter, [a1, a2, a3, a4, a5, a6, a7])
-  #   exp_anns = [a1, a2, a3, a6, a7]
-  #   assert_equal(exp_anns.sort, anns.sort)
-  # end
+  def test_get_keyword_annotations
+    input = 'melanoma, white blood cell, arm, cavity of stomach, melanoma'
+    delimiter = ','
+    a1 = @@custom_annotation.new(1, 8, 'PREF', 'MELANOMA', nil)
+    a2 = @@custom_annotation.new(11, 26, 'PREF', 'WHITE BLOOD CELL', nil)
+    a3 = @@custom_annotation.new(29, 31, 'SYN', 'ARM', nil)
+    a4 = @@custom_annotation.new(17, 26, 'SYN', 'BLOOD CELL', nil)
+    a5 = @@custom_annotation.new(17, 21, 'PREF', 'BLOOD', nil)
+    a6 = @@custom_annotation.new(34, 50, 'PREF', 'CAVITY OF STOMACH', nil)
+    a7 = @@custom_annotation.new(53, 60, 'PREF', 'MELANOMA', nil)
+    anns = OntologyRecommender::Utils::AnnotatorUtils.get_keyword_annotations(input, delimiter, [a1, a2, a3, a4, a5, a6, a7])
+    exp_anns = [a1, a2, a3, a6, a7]
+    assert_equal(exp_anns.sort, anns.sort)
+  end
 
-  # def test_get_annotations_for_fragment
-  #   a1 = @@custom_annotation.new(1, 8, 'PREF', 'MELANOMA', 'o1_uri', 'o1_acronym', 'o1_uri/melanoma', nil)
-  #   a2 = @@custom_annotation.new(11, 26, 'PREF', 'WHITE BLOOD CELL', 'o1_uri', 'o1_acronym', 'o1_uri/wbc', nil)
-  #   a3 = @@custom_annotation.new(29, 36, 'PREF', 'MELANOMA', 'o1_uri', 'o1_acronym', 'o1_uri/melanoma', nil)
-  #   a4 = @@custom_annotation.new(43, 45, 'SYN', 'ARM', 'o1_uri', 'o1_acronym', 'o1_uri/arm', nil)
-  #   a5 = @@custom_annotation.new(40, 55, 'PREF', 'WHITE BLOOD CELL', 'o1_uri', 'o1_acronym', 'o1_uri/wbc', nil)
-  #   a6 = @@custom_annotation.new(17, 26, 'SYN', 'BLOOD CELL', 'o2_uri', 'o2_acronym', 'o2_uri/bc', nil)
-  #   a7 = @@custom_annotation.new(17, 21, 'PREF', 'BLOOD', 'o3_uri', 'o3_acronym', 'o3_uri/blood', nil)
-  #   anns = OntologyRecommender::Utils::AnnotatorUtils.get_annotations_for_fragment(17, 21, [a1, a2, a3, a4, a5, a6, a7])
-  #   exp_anns = [a6, a7]
-  #   assert_equal(exp_anns.sort, anns.sort)
-  # end
+  def test_get_annotations_for_fragment
+    a1 = @@custom_annotation.new(1, 8, 'PREF', 'MELANOMA', nil)
+    a2 = @@custom_annotation.new(11, 26, 'PREF', 'WHITE BLOOD CELL', nil)
+    a3 = @@custom_annotation.new(29, 36, 'PREF', 'MELANOMA', nil)
+    a4 = @@custom_annotation.new(43, 45, 'SYN', 'ARM', nil)
+    a5 = @@custom_annotation.new(40, 55, 'PREF', 'WHITE BLOOD CELL', nil)
+    a6 = @@custom_annotation.new(17, 26, 'SYN', 'BLOOD CELL', nil)
+    a7 = @@custom_annotation.new(17, 21, 'PREF', 'BLOOD', nil)
+    anns = OntologyRecommender::Utils::AnnotatorUtils.get_annotations_for_fragment(17, 21, [a1, a2, a3, a4, a5, a6, a7])
+    exp_anns = [a6, a7]
+    assert_equal(exp_anns.sort, anns.sort)
+  end
 
   def test_get_keyword_positions
     delimiter = ','
