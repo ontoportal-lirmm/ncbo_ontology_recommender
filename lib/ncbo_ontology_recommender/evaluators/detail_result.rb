d04449bc57e24c9df51cdc7382528f113538b4a4
Marcos Martinez <marcos.martinez.romero@gmail.com>
2015-03-12 22:45:18 -0700

    Add variables for definitions, synonnyms and properties scores and change name of score variable to normalizedScore

--- lib/ncbo_ontology_recommender/evaluators/detail_result.rb
+++ lib/ncbo_ontology_recommender/evaluators/detail_result.rb
@@ -6,10 +6,13 @@ module OntologyRecommender
     #
     class DetailResult
 
-      attr_reader :score
+      attr_reader :normalizedScore, :definitionsScore, :synonymsScore, :propertiesScore
 
-      def initialize(score)
-        @score = score
+      def initialize(normalized_score, definitions_score, synonynms_score, properties_score)
+        @normalizedScore = normalized_score
+        @definitionsScore = definitions_score
+        @synonymsScore = synonynms_score
+        @propertiesScore = properties_score
       end
 
     end
