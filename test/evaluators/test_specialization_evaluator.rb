44fb788731a27e1fd9a39dedbd9c37989448116c
Marcos Martinez <marcos.martinez.romero@gmail.com>
2015-03-17 17:18:51 -0700

    Minor changes

--- test/evaluators/test_specialization_evaluator.rb
+++ test/evaluators/test_specialization_evaluator.rb
@@ -1,29 +1,29 @@
 require_relative '../test_case'
 require_relative '../../lib/ncbo_ontology_recommender/evaluators/specialization_evaluator'
-require_relative '../../lib/ncbo_ontology_recommender/utils/utils'
+require_relative '../../lib/ncbo_ontology_recommender/helpers/general_helper'
 
 class TestSpecializationEvaluator < TestCase
 
   def self.before_suite
+    pref_score = 10
+    syn_score = 5
+    multiterm_score = 4
+    @@specialization_evaluator = OntologyRecommender::Evaluators::SpecializationEvaluator.new(pref_score, syn_score, multiterm_score)
   end
 
   def self.after_suite
   end
 
   def test_evaluate
-    pref_score = 10
-    syn_score = 5
-    multiterm_score = 4
-    specialization_evaluator = OntologyRecommender::Evaluators::SpecializationEvaluator.new(pref_score, syn_score, multiterm_score)
     input = 'software, hormone'
     # Expected annotations:
     # - MCCLTEST-0 -> hormone
     # - BROTEST-0 -> software
-    annotations = OntologyRecommender::Utils::AnnotatorUtils.get_annotations(input, 2, ',', [])
+    annotations = OntologyRecommender::Helpers::AnnotatorHelper.get_annotations(input, 2, ',', [])
     annotations_hash = annotations.group_by{|ann| ann.annotatedClass.submission.ontology.acronym}
-    result1 = specialization_evaluator.evaluate(annotations_hash, 'MCCLTEST-0')
-    result2 = specialization_evaluator.evaluate(annotations_hash, 'BROTEST-0')
-    result3 = specialization_evaluator.evaluate(annotations_hash, 'ONTOMATEST-0')
+    result1 = @@specialization_evaluator.evaluate(annotations_hash, 'MCCLTEST-0')
+    result2 = @@specialization_evaluator.evaluate(annotations_hash, 'BROTEST-0')
+    result3 = @@specialization_evaluator.evaluate(annotations_hash, 'ONTOMATEST-0')
     assert(result1.score > 0)
     assert(result1.normalizedScore > 0)
     assert(result2.score > 0)
@@ -31,6 +31,12 @@ class TestSpecializationEvaluator < TestCase
     # The result for the ontology that does not provide any annotations must be zero
     assert_equal(0, result3.score)
     assert_equal(0, result3.normalizedScore)
+  end
+
+  def test_get_number_of_classes
+    assert_equal(810, @@specialization_evaluator.send(:get_number_of_classes, 'MCCLTEST-0'))
+    assert_equal(486, @@specialization_evaluator.send(:get_number_of_classes, 'BROTEST-0'))
+    assert_equal(435, @@specialization_evaluator.send(:get_number_of_classes, 'ONTOMATEST-0'))
   end
 
 end
