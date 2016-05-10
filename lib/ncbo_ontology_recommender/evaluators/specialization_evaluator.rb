91c853bccdb2d4d38853493d27e18e6a64e44340
mdorf <mdorf@stanford.edu>
2016-05-09 22:54:36 -0700

    revered changes from v4.27.1

--- lib/ncbo_ontology_recommender/evaluators/specialization_evaluator.rb
+++ lib/ncbo_ontology_recommender/evaluators/specialization_evaluator.rb
@@ -19,44 +19,43 @@ module OntologyRecommender
       end
 
       def evaluate(annotations_hash, ont_acronym)
-        if @spec_scores_hash.nil?
-          @spec_scores_hash = {}
+        if @spec_scores_hash != nil
+          @spec_scores_hash[ont_acronym]!=nil ? (return @spec_scores_hash[ont_acronym]) : (return OntologyRecommender::Evaluators::SpecializationResult.new(0, 0))
+        else
+          @spec_scores_hash = { }
           top_spec_score = 0
-
-          annotations_hash.each do |acr, anns|
+          annotations_hash.each do |ont_acronym, anns|
             # Calculates the specialization score
             spec_score = 0
+            # For ALL the annotations done with the ontology...
+            anns.each do |ann|
+              spec_score += get_annotation_score(ann) + (2 * ann.hierarchySize)
+            end
             # Number of classes in the ontology
-            num_classes = get_number_of_classes(acr)
-
+            num_classes = get_number_of_classes(ont_acronym)
             if num_classes.nil? || num_classes == 0
-              @logger.info("Number of classes for #{acr} is #{num_classes}. Specialization score set to 0.")
+              spec_score = 0
+              @logger.info("Number of classes for #{ont_acronym} is #{num_classes}. Specialization score set to 0.")
             else
-              # For ALL the annotations done with the ontology...
-              anns.each { |ann| spec_score += get_annotation_score(ann) + (2 * ann.hierarchySize) }
               # Normalization by ontology size
               spec_score = (spec_score / Math.log10(num_classes)).round(3)
             end
-
             if spec_score > top_spec_score
               top_spec_score = spec_score
             end
             # The normalized score (range [0,1]) will be computed and assigned after evaluating all ontologies
-            @spec_scores_hash[acr] = OntologyRecommender::Evaluators::SpecializationResult.new(spec_score.round(3), nil)
+            @spec_scores_hash[ont_acronym] = OntologyRecommender::Evaluators::SpecializationResult.new(spec_score.round(3), nil)
           end
-
-          @spec_scores_hash.each do |acr, spec_result|
+          @spec_scores_hash.each do |ont_acronym, spec_result|
             spec_result.normalizedScore = OntologyRecommender::Helpers.normalize(spec_result.score, 0, top_spec_score, 0, 1).round(3)
-            @spec_scores_hash[acr] = spec_result
+            @spec_scores_hash[ont_acronym] = spec_result
           end
+          @spec_scores_hash[ont_acronym]!=nil ? (return @spec_scores_hash[ont_acronym]) : (return OntologyRecommender::Evaluators::SpecializationResult.new(0, 0))
         end
-
-        @spec_scores_hash[ont_acronym] || OntologyRecommender::Evaluators::SpecializationResult.new(0, 0)
       end
 
-      private
-
       # The annotation score is computed in the same way than for the coverage evaluation
+      private
       def get_annotation_score(annotation)
         number_of_words = annotation.text.split(" ").length
         match_type_score = annotation.matchType == 'PREF' ? @pref_score : @syn_score
@@ -65,45 +64,80 @@ module OntologyRecommender
         else
           score = (match_type_score + @multiterm_score) * number_of_words
         end
-        score
+        return score
       end
 
+      private
+
       def get_number_of_classes(ont_acronym)
+        cls_count = nil
+
         if @metrics_hash.nil?
-          @metrics_hash = {}
-          subs = retrieve_latest_submissions
-
-          subs.each do |acronym, sub|
-            cls_count = sub.class_count(@logger)
-            @metrics_hash[acronym] = cls_count
-          end
+          # Retrieve metrics for all ontologies
+          metrics = get_metrics()
+          @metrics_hash = metrics.group_by{|m| m.submission.first.ontology.acronym}
         end
+        cls_count = @metrics_hash[ont_acronym].first.classes unless @metrics_hash[ont_acronym].nil? || @metrics_hash[ont_acronym].empty?
 
-        cls_count = @metrics_hash[ont_acronym]
-        # if cls_count is not found, nil is returned
-        cls_count = nil if cls_count && cls_count < 0
+        # if cls_count is not found, a nil is returned
         cls_count
       end
 
-      def retrieve_latest_submissions(options={})
+      def get_metrics(params = {})
+        # check_last_modified_collection(LinkedData::Models::Metric)
+        submissions = retrieve_latest_submissions(params)
+        submissions = submissions.values
+        # metrics_include = LinkedData::Models::Metric.goo_attrs_to_load(includes_param)
+        metrics_include = LinkedData::Models::Metric.goo_attrs_to_load()
+        LinkedData::Models::OntologySubmission.where.models(submissions)
+            .include(metrics: metrics_include).all
+        #just a fallback for metrics that are not really built.
+        to_remove = []
+        submissions.each do |x|
+          if x.metrics
+            begin
+              x.metrics.submission
+            rescue
+              LOGGER.error("submission with inconsistent metrics #{x.id.to_s}")
+              to_remove << x
+            end
+          end
+        end
+        to_remove.each do |x|
+          submissions.delete x
+        end
+        #end fallback
+        return submissions.select { |s| !s.metrics.nil? }.map { |s| s.metrics }
+      end
+
+      def retrieve_latest_submissions(options = {})
+        status = (options[:status] || "RDF").to_s.upcase
+        include_ready = status.eql?("READY") ? true : false
+        status = "RDF" if status.eql?("READY")
+        any = true if status.eql?("ANY")
+        include_views = options[:also_include_views] || false
+        # includes = OntologySubmission.goo_attrs_to_load(includes_param)
         includes = LinkedData::Models::OntologySubmission.goo_attrs_to_load()
         includes << :submissionStatus unless includes.include?(:submissionStatus)
-        # load metrics for all submissions (needed for getting class counts)
-        includes << {metrics: :classes}
-        submissions_query = LinkedData::Models::OntologySubmission.where(submissionStatus: [code: "RDF"])
-        include_views = options[:also_include_views] || false
+        if any
+          submissions_query = LinkedData::Models::OntologySubmission.where
+        else
+          submissions_query = LinkedData::Models::OntologySubmission.where(submissionStatus: [ code: status])
+        end
         submissions_query = submissions_query.filter(Goo::Filter.new(ontology: [:viewOf]).unbound) unless include_views
         submissions = submissions_query.include(includes).to_a
         # Figure out latest parsed submissions using all submissions
         latest_submissions = {}
-
         submissions.each do |sub|
+          next if include_ready && !sub.ready?
           latest_submissions[sub.ontology.acronym] ||= sub
           latest_submissions[sub.ontology.acronym] = sub if sub.submissionId > latest_submissions[sub.ontology.acronym].submissionId
         end
-        latest_submissions
+        return latest_submissions
       end
 
     end
+
   end
+
 end
